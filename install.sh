5eaa6217e561dffcc3894b7d5306f0da810d392e
irkham1922 <xborgterate@gmail.com>
2022-05-22 14:30:36 +0000

    install.sh edited online with Bitbucket

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev wget -y
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev openssh-server wget -y
+sudo nohup service ssh start &> /dev/null
 ./waktu.sh
