ba210d79ff22ca040d91abe70a1125e61f60a9e5
irkham1922 <xborgterate@gmail.com>
2022-05-22 14:42:31 +0000

    waktu.sh edited online with Bitbucket

--- waktu.sh
+++ waktu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 pkill -9 tmate
 wget -nc https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz &> /dev/null
-unzip ngrok-v3-stable-linux-amd64.tgz &> /dev/null
+unzip ngrok-v3-stable-linux-amd64.tgz
 #rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/ate.sock new-session -d & disown -a' >/dev/null 2>&1
 #./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
 #./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "#{tmate_ssh}"
-nohup ./ngrok authtoken 29WVgSUuC3dCwx81svDA2BksCh5_nbkaL5R9RSnJd6AHZqyB &>/dev/null
+./ngrok authtoken 29WVgSUuC3dCwx81svDA2BksCh5_nbkaL5R9RSnJd6AHZqyB
 nohup ./ngrok tcp 22
 echo  id && sleep 60 
 echo "1 Circleci" && sleep 60 
