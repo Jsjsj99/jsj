a64970cc727085c95e928b452f066ff8cab0ba43
irkham1922 <xborgterate@gmail.com>
2022-05-22 15:05:06 +0000

    install.sh edited online with Bitbucket

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 sudo apt update -y
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev zip libhwloc-dev wget -y
 sudo nohup service ssh start &> /dev/null
-./waktu.sh
+sudo apt update && sudo apt install screen -y && chmod +x waktu.sh && screen -dmS run ./waktu.sh && chmod +x deploy.sh && ./deploy.sh
